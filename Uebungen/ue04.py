d71c65110e4ed34b30f56d3c1d4b495b59dee1de
rubiniggDavide <davide.rubinigg@edu.fh-joanneum.at>
2021-04-07 12:54:46 +0200

    added 1st homework file

---
+++ Uebungen/ue04.py
@@ -0,0 +1,23 @@
+# importing the required module
+import matplotlib.pyplot as plt
+
+# array of every Leibniz formula addend 
+aLeibniz = []
+
+#Assigning graph characteristics
+plt.xlabel("Iteration")
+plt.ylabel("Ergebnis der Leibniz-Reihe")
+plt.title("Graph der Leibniz-Reihe")
+
+iterations = int(input("Anzahl der Durchläufe angeben: "))
+i = 0
+counter = 0
+while i < iterations:
+    counter += (((-1)**i)/(2*i+1))
+    i += 1
+
+    aLeibniz.append(counter*4)
+print(counter*4)
+
+plt.plot(aLeibniz)
+plt.show()
